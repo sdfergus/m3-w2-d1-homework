b9a9b0c4ecf841ac6e581cbd02d4e8c183257b05
Shandira <shandira.ferguson@gmail.com>
2022-11-04 11:44:19 -0700

    Given data successfully added to collection

--- app.js
+++ app.js
@@ -12,56 +12,60 @@ MongoClient.connect( url, {
     // db.close();
 
     var dbo = db.db( "statsdb" );
-    dbo.createCollection( "uscensus", function ( err, res ) {
+    // dbo.createCollection( "uscensus", function ( err, res ) {
+    //     if ( err ) throw err;
+    //     console.log( "uscensus collection created!" );
+    //     db.close();
+    // } );
+
+    var stats = [
+        {
+            'city': 'San Juan',
+            'zip': '00926',
+            'state': 'PR',
+            'income': '34781',
+            'age': '44'
+        },
+        {
+            'city': 'Corona',
+            'zip': '11368',
+            'state': 'NY',
+            'income': '50797',
+            'age': '32'
+        },
+        {
+            'city': 'Chicago',
+            'zip': '60629',
+            'state': 'IL',
+            'income': '42019',
+            'age': '31'
+        },
+        {
+            'city': 'El Paso',
+            'zip': '79936',
+            'state': 'TX',
+            'income': '54692',
+            'age': '31'
+        },
+        {
+            'city': 'Los Angeles',
+            'zip': '90011',
+            'state': 'CA',
+            'income': '36954',
+            'age': '28'
+        },
+        {
+            'city': 'Norwalk',
+            'zip': '90650',
+            'state': 'CA',
+            'income': '66453',
+            'age': '35'
+        }
+    ]
+
+    dbo.collection( "uscensus" ).insertMany( stats, function ( err, res ) {
         if ( err ) throw err;
-        console.log( "uscensus collection created!" );
+        console.log( "Stats array objects inserted! Length: " + res.insertedCount );
         db.close();
-    } );
+    } )
 } );
-
-
-
-// var stats = [
-//     {
-//         'city': 'San Juan',
-//         'zip': '00926',
-//         'state': 'PR',
-//         'income': '34781',
-//         'age': '44'
-//     },
-//     {
-//         'city': 'Corona',
-//         'zip': '11368',
-//         'state': 'NY',
-//         'income': '50797',
-//         'age': '32'
-//     },
-//     {
-//         'city': 'Chicago',
-//         'zip': '60629',
-//         'state': 'IL',
-//         'income': '42019',
-//         'age': '31'
-//     },
-//     {
-//         'city': 'El Paso',
-//         'zip': '79936',
-//         'state': 'TX',
-//         'income': '54692',
-//         'age': '31'
-//     },
-//     {
-//         'city': 'Los Angeles',
-//         'zip': '90011',
-//         'state': 'CA',
-//         'income': '36954',
-//         'age': '28'
-//     },
-//     {
-//         'city': 'Norwalk',
-//         'zip': '90650',
-//         'state': 'CA',
-//         'income': '66453',
-//         'age': '35'
-//     }
-// ]
